80a241cc075b364f3dea8b0433d2e53e4e332005
Alexander45-star <Lizredy@yandex.ru>
2024-02-22 19:16:12 +0400

    Update ParsingDerective

--- src/AST/DerectiveStatement/ParsingDerective.js
+++ src/AST/DerectiveStatement/ParsingDerective.js
@@ -1,2 +1,15 @@
 import program from "../../AST/ast.js";
 
+class ParsingDerective {
+    parse_derective(code_derective, position) {
+        program.body.ParsingDerective = {
+            type: "ParsingDerective",
+            start: position,
+            end: code_derective.length,
+            kind: code_derective.replace(/\@/, "")
+        }
+        console.log(program)
+    }
+}
+
+export default ParsingDerective;
--- src/parser/reading/read.js
+++ src/parser/reading/read.js
@@ -1,6 +1,6 @@
 import fs from "node:fs"
-import ParsingFunction from "../../AST/FunctionDeclaration/ParsingFunction.js";
+import ParsingDerective from "../../AST/DerectiveStatement/ParsingDerective.js";
 
 let teaConfig = JSON.parse(fs.readFileSync("../../../test/teaconfig.json", { encoding: "utf-8" }));
 let code = fs.readFileSync(`../../../test/${teaConfig.file}`, { encoding: "utf-8" });
-new ParsingFunction().parse_function(code, 9);
+new ParsingDerective().parse_derective(code, 0);
